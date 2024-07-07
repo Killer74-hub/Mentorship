a4e5ce299333d18bc91c38ee973fc73b71ab4141
Killer74-hub <126610656+Killer74-hub@users.noreply.github.com>
2024-07-07 19:57:27 +0200

    Added the Main Test File
    This is the Main File for a Spotify downloader with the library spotdl. Change the URL to your Playlist or Music track. After " --output "Put the path. No Download restrictions

---
+++ Downloader.py
@@ -0,0 +1,5 @@
+import spotdl
+
+spotdl https://open.spotify.com/track/6jlG8gBPNAgBgoivw2Ig09?si=1yozzpJJRPmzQp51Km0NvA&context=spotify%3Atrack%3A6jlG8gBPNAgBgoivw2Ig09 --output Downloads
+
+print("Downloaded the track.:)")
