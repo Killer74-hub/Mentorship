f450dfea36822c9bf212e910352a1f6082f3e2fe
Killer74-hub <126610656+Killer74-hub@users.noreply.github.com>
2024-07-07 19:54:14 +0200

    Delete Downloader.py

--- Downloader.py
+++
@@ -1,5 +0,0 @@
-import spotdl
-
-spotdl https://open.spotify.com/track/6jlG8gBPNAgBgoivw2Ig09?si=1yozzpJJRPmzQp51Km0NvA&context=spotify%3Atrack%3A6jlG8gBPNAgBgoivw2Ig09 --output Downloads
-
-print("Downloaded the track.:)")
